a583c7f84d83a8517f435e6649c39c7df5ef967d
Nariman <n.murzakaev@big3.ru>
2023-03-12 15:11:02 +0300

    added task description

--- packages/module-3-react/src/components/UseHover/index.tsx
+++ packages/module-3-react/src/components/UseHover/index.tsx
@@ -9,4 +9,16 @@ export function UseHover() {
       {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
     </div>
   );
+
+  // import { useHover } from './useHover';
+
+  // function Demo() {
+  //   const { hovered, ref } = useHover();
+
+  //   return (
+  //     <div ref={ref}>
+  //       {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+  //     </div>
+  //   );
+  // }
 }
