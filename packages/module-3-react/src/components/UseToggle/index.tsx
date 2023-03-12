4a4ec0d0e8b51d0b0b80c51f8ba338db265cbeef
Nariman <n.murzakaev@big3.ru>
2023-03-12 15:17:31 +0300

    added task description

--- packages/module-3-react/src/components/UseToggle/index.tsx
+++ packages/module-3-react/src/components/UseToggle/index.tsx
@@ -8,4 +8,27 @@ export function UseToggle() {
       {value}
     </button>
   );
+
+  // import { useToggle } from './useToggle';
+
+  // function Demo() {
+  //   const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
+
+  //   return (
+  //     <button onClick={() => toggle()}>
+  //       {value}
+  //     </button>
+  //   );
+  // }
+
+  // // Еще примеры использования
+
+  // const [value, toggle] = useToggle(['light', 'dark']);
+
+  // toggle(); // -> value === 'light'
+  // toggle(); // -> value === 'dark'
+
+  // // Так же можно передать конкретное значение и тогда 
+  // // value станет одним из значений
+  // toggle('dark'); // -> value === 'dark'
 }
